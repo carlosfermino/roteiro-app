db56970cf164e87d2a70ab06c63837cb24abbb78
Carlos Fermino <brutosacf@gmail.com>
2024-08-26 19:10:09 -0300

    Terminado a HomePage do app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
-import { StyleSheet, View, Text, StatusBar } from 'react-native';
+import { StyleSheet, View, Text, StatusBar, TextInput, Platform, Pressable } from 'react-native';
+import {MaterialIcons} from '@expo/vector-icons'
+import Slider from '@react-native-community/slider';
+
+const statusBarHeight = StatusBar.currentHeight
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Teste 1234</Text>
       <StatusBar barStyle='dark-content' translucent={true} backgroundColor="#F1F1F1" />
+      <Text style={styles.heading}>Roteiros Fácil</Text>
+      <View style={styles.form}>
+        <Text style={styles.label}>Cidade destino</Text>
+        <TextInput placeholder="Ex: Sâo Sebastião, SP" style={styles.input} />
+        <Text style={styles.label}>Tempo de estadia: <Text style={styles.days}> 10 </Text> dias</Text>
+        <Slider
+          //style={{width: 200, height: 40}}
+          minimumValue={1}
+          maximumValue={7}
+          minimumTrackTintColor="#009688"
+          maximumTrackTintColor="#000000"
+        />
+      </View>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Gerar roteiro</Text>
+        <MaterialIcons name='travel-explore' size={24} color='#fff' />
+      </Pressable>
     </View>
   );
 }
@@ -12,8 +32,53 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F1F1F1',
     alignItems: 'center',
-    justifyContent: 'center'
+    paddingTop: 40,
   },
+  heading: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    paddingTop: Platform.OS === 'android' ? statusBarHeight : 54,
+  },
+  form: {
+    backgroundColor: '#fff',
+    width: '90%',
+    borderRadius: 8,
+    padding: 16, 
+    marginTop: 16,
+    marginBottom: 8,
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+
+  }, 
+  input: {
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: '#94a3b8',
+    padding: 8,
+    fontSize: 16,
+    marginBottom: 16,
+  },
+  days: {
+    backgroundColor: '#F1F1F1',
+  },
+  button: {
+    backgroundColor: '#FF5656',
+    width: '90%',
+    borderRadius: 8,
+    flexDirection: 'row',
+    padding: 14,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 8
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: 'bold',
+  }
 });
